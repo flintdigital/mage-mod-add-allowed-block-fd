4ecf69c4c5743af4895a059b624a7d1646fec8fa
Nathan Flint <nate@flintdigital.com>
2015-11-06 19:31:29 -0700

    Cleaning up the code

--- app/code/local/FlintDigital/AddAllowedBlockType/sql/addallowedblocktype_setup/install-1.0.0.php
+++ app/code/local/FlintDigital/AddAllowedBlockType/sql/addallowedblocktype_setup/install-1.0.0.php
@@ -1,47 +1,35 @@
 <?php
-/**
- * path magento_root/app/code/local/FlintDigital/AddAllowedBlockType/sql/flintdigital_addallowedblocktype/install-1.0.0.php
- */
- 
+
 $installer = $this;
- 
 $installer->startSetup();
- 
-/**
- * Adding allowed blocks to Mage_Core_Model_Email_Template_Filter after SUPEE-6788 install
- */
+
+
 $allowedBlocksArray = array(
-    'cms/block',
-    'custom/block_type'
+	'cms/block',
+	'custom/block_type'
 );
- 
+
 foreach ($allowedBlocksArray as $item) {
-    try {
-        Mage::getModel('admin/block')->setData('block_name', $item)
-            ->setData('is_allowed', 1)
-            ->save()
-        ;
-    } catch(Exception $e) {
-        Mage::log($e->getMessage(), null, 'flintdigital_add_allowed.log', true);
-    }
+	try {
+		Mage::getModel('admin/block')->setData('block_name', $item)->setData('is_allowed', 1)->save();
+	}
+	catch (Exception $e) {
+		Mage::log($e->getMessage(), null, 'flintdigital_add_allowed.log', true);
+	}
 }
- 
-/**
- * Adding allowed blocks to Mage_Core_Model_Email_Template_Filter after SUPEE-6788 install
- */
+
+
 $allowedConfigArray = array(
-    'custom/config/path',
+	'custom/config/path'
 );
- 
+
 foreach ($allowedConfigArray as $item) {
-    try {
-        Mage::getModel('admin/variable')->setData('variable_name', $item)
-            ->setData('is_allowed', 1)
-            ->save()
-        ;
-    } catch(Exception $e) {
-        Mage::log($e->getMessage(), null, 'flintdigital_add_allowed.log', true);
-    }
+	try {
+		Mage::getModel('admin/variable')->setData('variable_name', $item)->setData('is_allowed', 1)->save();
+	}
+	catch (Exception $e) {
+		Mage::log($e->getMessage(), null, 'flintdigital_add_allowed.log', true);
+	}
 }
- 
+
 $installer->endSetup();
